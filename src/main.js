184b5493f2d7856a67c7155e01c1ae0d90611905
calebbabin <object.caleb@gmail.com>
2020-12-12 21:56:33 -0600

    add basic elements

--- src/main.js
+++ src/main.js
@@ -16,6 +16,9 @@ const ChatInstance = new Chat({
 	duplicateEmoteLimit_pleb: 0,
 })
 
+const emoteSize = 56;
+const PI2 = Math.PI * 2;
+
 const emoteTextures = {};
 const pendingEmoteArray = [];
 ChatInstance.on("emotes", (e) => {
@@ -28,17 +31,55 @@ ChatInstance.on("emotes", (e) => {
 		emote.texture = emoteTextures[emote.material.id];
 		output.emotes.push(emote);
 	}
-	pendingEmoteArray.push(output);
+	const direction = Math.random() * PI2;
+	pendingEmoteArray.push({
+		position: { x: main.position.x, y: main.position.y },
+		velocity: { x: Math.sin(direction), y: Math.cos(direction) },
+		height: emoteSize,
+		width: emoteSize * output.emotes.length,
+		emotes: output.emotes,
+		life: 0,
+	});
 })
 
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 
+const mainDirection = Math.random() * PI2;
+const main = {
+	position: { x: 0, y: 0 },
+	velocity: { x: Math.sin(mainDirection), y: Math.cos(mainDirection) },
+	width: emoteSize * 4,
+	height: emoteSize * 4,
+}
+
+const bounce = (element) => {
+	if (element.position.x + element.width >= window.innerWidth) {
+		element.position.x = window.innerWidth - (element.width + 0.0001);
+		element.velocity.x *= -1;
+	}
+	if (element.position.y + element.height >= window.innerHeight) {
+		element.position.y = window.innerHeight - (element.height + 0.0001);
+		element.velocity.y *= -1;
+	}
+	if (element.position.x <= 0) {
+		element.position.x = 0.0001;
+		element.velocity.x *= -1;
+	}
+	if (element.position.y <= 0) {
+		element.position.y = 0.0001;
+		element.velocity.y *= -1;
+	}
+}
+
 
 window.addEventListener('DOMContentLoaded', () => {
+	function resize() {
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+	}
 	function init() {
-		window.addEventListener('resize', () => {
-		})
+		window.addEventListener('resize', resize)
 		document.body.appendChild(canvas);
 	}
 
@@ -47,8 +88,46 @@ window.addEventListener('DOMContentLoaded', () => {
 		requestAnimationFrame(draw);
 		const delta = (Date.now() - lastFrame) / 1000;
 		lastFrame = Date.now();
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
 
+		for (let index = pendingEmoteArray.length - 1; index >= 0; index--) {
+			const element = pendingEmoteArray[index];
+			element.position.x += element.velocity.x * delta * 150;
+			element.position.y += element.velocity.y * delta * 150;
+
+			bounce(element);
+
+			element.life += delta;
+			for (let i = 0; i < element.emotes.length; i++) {
+				const emote = element.emotes[i];
+				ctx.drawImage(emote.material.canvas,
+					(element.position.x + emoteSize * i),
+					(element.position.y),
+					(emoteSize),
+					(emoteSize * (emote.material.canvas.height / emote.material.canvas.width)));
+			}
+
+			if (element.life > 5) {
+				pendingEmoteArray.splice(index, 1);
+			}
+		}
+
+		bounce(main);
+		main.position.x += main.velocity.x * delta * 100;
+		main.position.y += main.velocity.y * delta * 100;
+		ctx.fillStyle = '#ff0000';
+		ctx.fillRect(
+			main.position.x,
+			main.position.y,
+			main.width,
+			main.height,
+		)
 	}
+
+	resize();
+		
+	main.position.x = canvas.width/2 - main.width/2;
+	main.position.y = canvas.height/2 - main.height/2;
 
 	init();
 	draw();
